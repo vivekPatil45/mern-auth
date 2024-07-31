764233d2cddaf85c640b180e85962043d1ffc777
Vivek Patil <vivekrp4503@gmail.com>
2024-07-31 11:09:14 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,9 @@
+import { errorHandler } from '../error.js';
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+
 
 export const signup = async (req, res,next) => {
     const { username, email, password } = req.body;
@@ -8,6 +12,37 @@ export const signup = async (req, res,next) => {
     try {
         await newUser.save();
         res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        next(error);
+        // res.status(500).json(error.message);
+    }
+};
+
+
+export const signin = async (req,res,next) => {
+    const { email, password } = req.body;
+
+
+    
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, 'User not found'));
+        }
+
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401, 'wrong credentials'));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+        res
+            .cookie('access_token', token, { httpOnly: true, expires: expiryDate }) //httponly to prevent the third party app modify the cookies
+            .status(200)
+            .json(rest);
+
+
     } catch (error) {
         next(error);
         // res.status(500).json(error.message);
