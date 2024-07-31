67256d5f0788f465ff32f5278a79d6bdbccef206
Vivek Patil <vivekrp4503@gmail.com>
2024-07-31 17:28:48 +0530

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
-const app = express();
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
+const app = express();
+const __dirname = path.resolve();
+
+
 
 mongoose
     .connect(process.env.MONGO)
@@ -17,6 +21,11 @@ mongoose
     console.log(err);
 });
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 app.use(express.json());
 app.use(cookieParser());
 
